36e9fa8c5b72221674d230618b035486514a66e9
Eko Setyobudi <eko.setyobudi@kpiinstitute.org>
2022-02-16 16:10:39 +0700

    Update some method in user.service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,23 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './interfaces/user.interface';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel('User') private UserModel: Model<User>) {}
+
+  async create(createUserDto: CreateUserDto): Promise<any> {
+    const data = new this.UserModel(createUserDto);
+
+    return await data.save();
   }
 
   findAll() {
     return `This action returns all user`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(_id: string): Promise<User> {
+    const data = await this.UserModel.findById(_id).exec();
+
+    return data;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(_id: string, updateUserDto: UpdateUserDto): Promise<any> {
+    return await this.UserModel.findByIdAndUpdate(_id, updateUserDto, {
+      new: true,
+    });
   }
 
   remove(id: number) {
